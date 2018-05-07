26f9bbe76d7a5ef5511dbd4972e27ea6e52f8538
petercolella <peter.joseph.colella@gmail.com>
2018-05-07 01:13:40 -0400

    scrub click working

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('progress__filled');
+const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
@@ -28,12 +28,24 @@ function skip() {
 
 function handleRangeUpdate() {
     video[this.name] = this.value;
-    console.log (this.value);
+    // console.log (this.value);
+}
+
+function handleProgressUpdate() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    console.log(scrubTime);
 }
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgressUpdate);
 
 toggle.addEventListener('click', togglePlay);
 
@@ -41,3 +53,5 @@ skipButtons.forEach(button => button.addEventListener('click', skip));
 
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+progress.addEventListener('click', scrub);
