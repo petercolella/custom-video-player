0018e201385cc187f03671ba612e1f086621f166
petercolella <peter.joseph.colella@gmail.com>
2018-05-06 23:57:49 -0400

    play pause icon working

--- scripts.js
+++ scripts.js
@@ -15,3 +15,14 @@ function togglePlay() {
     const method = video.paused ? 'play' : 'pause';
     video[method]();
 }
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
